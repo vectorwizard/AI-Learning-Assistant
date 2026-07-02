aef0f74d2591e5467731b2923fe01c192e1c0da5
Arijit <arijitroylochan@gmail.com>
2026-07-02 15:19:25 +0530

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ connectDB();
 //middlewares to handle CORS
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://ai-learning-assistant-ruby-chi.vercel.app",'http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
